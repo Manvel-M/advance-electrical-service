f96cc87cf2740f25ee1b296b31e0c6c05d4dc2c9
Manvel-M <manveldev@gmail.com>
2025-08-01 08:11:35 -0700

    feat: Implement Button component with variant and size support using Tailwind CSS

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,53 @@
+import { twMerge } from "tailwind-merge";
+import { cva, type VariantProps } from "class-variance-authority";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  {
+    variants: {
+      variant: {
+        default: "bg-foreground text-background hover:bg-foreground/80",
+        primary: "bg-primary text-primary-foreground hover:bg-primary/80",
+        destructive: "bg-error text-error-foreground hover:bg-error/80",
+
+        outline:
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:underline",
+      },
+      size: {
+        default: "h-10 px-4 py-2",
+        sm: "h-9 rounded-md px-3 text-sm",
+        lg: "h-11 rounded-md px-8",
+        icon: "h-10 w-10",
+      },
+      rounded: {
+        none: "rounded-none",
+        sm: "rounded-sm",
+        md: "rounded-md",
+        lg: "rounded-lg",
+        full: "rounded-full",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+      rounded: "md",
+    },
+  },
+);
+
+export function Button({
+  className,
+  variant,
+  size,
+  rounded,
+  ...props
+}: React.ComponentProps<"button"> & VariantProps<typeof buttonVariants>) {
+  return (
+    <button
+      className={twMerge(buttonVariants({ variant, size, rounded }), className)}
+      {...props}
+    />
+  );
+}
