f81afa32878936755cdc077ab02dad6039168a28
Manvel-M <manveldev@gmail.com>
2025-08-04 10:03:35 -0700

    feat: Add CallToAction section to enhance user engagement and contact options

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -2,4 +2,5 @@ import Header from "./header/Header.astro";
 import Hero from "./hero/Hero.astro";
 import Services from "./services/Services.astro";
 import Testimonials from "./testimonials/Testimonials.astro";
-export { Header, Hero, Services, Testimonials };
+import CallToAction from "./calltoaction/CallToAction.astro";
+export { Header, Hero, Services, Testimonials, CallToAction };
