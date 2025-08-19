a14287695e4b8b515083995bedcf13c58d4a8d96
Manvel-M <manveldev@gmail.com>
2025-08-19 13:15:56 -0700

    feat: Add company data and integrate into CallToAction and Footer components

---
+++ src/data/company.ts
@@ -0,0 +1,10 @@
+export const company = {
+  name: "Advance Electrical Pro Services",
+  street: "7916 Wentworth St",
+  city: "Sunland",
+  state: "CA",
+  zip: "91040",
+  phone: "818-416-3736",
+  email: "advanceelectricalserv@gmail.com",
+  license: "1080652",
+};
