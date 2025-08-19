1ccfa574e08c8249e569cc6290270f52b11a516b
Manvel-M <manveldev@gmail.com>
2025-08-19 13:22:56 -0700

    fix: Correct typo in MobileNav component CSS property

--- src/components/contact/Form.tsx
+++ src/components/contact/Form.tsx
@@ -1,4 +1,4 @@
-import { Form, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 // import { actions } from "astro:actions";
